94f7074a63096838e6a3c0e98ee319f210ab5149
Hypurrnating <imadyounis2004@gmail.com>
2023-07-19 21:21:28 +0300

    Update potato.py
    - added a single class for returns (yet)
    
    - plant stato
    - stain stato (limited to data and config. will do version history later)

--- potato.py
+++ potato.py
@@ -1,4 +1,4 @@
-import asyncio; import os; import sys; import json; import pathlib; import ast; import traceback
+import pathlib; import ast; import os; import sys; from zipfile import ZipFile, ZipInfo
 
 '''
 HTATO stands for HARD POTATO. These files must be created by the HTATO class, and has fixed keys.
@@ -9,6 +9,8 @@ STATO stands for SOFT POTATO. These can have a variable number of keys. Making i
 for files that might be used to store temporary data.
 
 Apart from the number of keys, HTATO and STATO have other characteristics that make it suitable for their use
+
+.potato files are for use by potato lib only (!)
 '''
 
 # Exceptions:
@@ -43,6 +45,14 @@ def CheckPotatoNonExistant(fn):
     return func
 
 
+class potato_returns():
+    class htato():
+        class HtatoPlantReturn():
+            def __init__(self, complete, path, keys):
+                self.complete = complete
+                self.path = path
+                self.keys = keys
+
 class POTATO():
     class STATO():
         def __init__(self, potato: pathlib.Path) -> None:
@@ -54,18 +64,47 @@ class POTATO():
                 raise NotSTATO('Not an STATO file')
 
         @CheckPotatoExistant
-        def PLANT(self, KEYS: list) -> list:
+        def PLANT(self, KEYS: list, version_history: bool = True, encryption: bool = False) -> list:
             """Creates a STATO file at the path and adds None to all keys. Users must update the keys using INJECT method.\n
             returns a list: [bool, potato path, keys in potato file]"""
 
+            if encryption == True:
+                print('Encryption is not currently fully supported')
+            configfile_string = str()
+            configfile_string += f'version_history: {version_history}' + '\n'
+            configfile_string += f'encryption: {encryption}' + '\n'
             potatofile_string = str()
             for x in KEYS:
                 potatofile_string += f'{x}: None' + '\n'
-            potatofile = open(self.potato, 'w')
-            potatofile.write(potatofile_string)
-            potatofile.close()
+            ZipFile(self.potato, 'w').close()
+            ZipFile(self.potato, 'a').writestr(data=configfile_string, zinfo_or_arcname='config.potato')
+            ZipFile(self.potato, 'a').writestr(data=potatofile_string, zinfo_or_arcname='data.txt')
+            ZipFile(self.potato, 'a').mkdir(zinfo_or_directory_name='version_history')
 
             return [True, self.potato, KEYS]
+
+        def STAIN(self, starch: str = 'data'):
+            """Reads STATO file. Starch specifies what part you want to read.\n
+            Type 'data' (default) for the simple dictionary stored inside, or 'config' for the preferences set during planting\n
+            returns a dictionary with keys and values of python objects"""
+
+            match starch:
+                case 'data':
+                    starch = 'data.txt'
+                case 'config':
+                    starch = 'config.potato'
+                case _:
+                    starch = starch + '.potato'
+            read = ZipFile(self.potato, 'r').read(name=starch).decode().split('\n')
+            parsed = dict()
+            for x in read:
+                if bool(x): #since its not .readlines() we need to make sure the string actually has something
+                    # python still sometimes read the \n ending.
+                    x = x.replace("\n", "").split(':')
+                    key = x[0].strip()
+                    value = ast.literal_eval(x[1].strip())
+                    parsed[key] = value
+            return parsed
 
 
     class HTATO():
@@ -78,9 +117,9 @@ class POTATO():
                 raise NotHTATO('Not an HTATO file')
 
         @CheckPotatoExistant
-        def PLANT(self, KEYS: list) -> list:
+        def PLANT(self, KEYS: list) -> potato_returns.htato.HtatoPlantReturn:
             """Creates a HTATO file at the path and adds None to all keys. Users must update the keys using INJECT method.\n
-            returns a list: [bool, potato path, keys in the potato file]"""
+            returns a plant_return class with parameters: complete, path, keys"""
 
             potatofile_string = str()
             for x in KEYS:
@@ -89,7 +128,7 @@ class POTATO():
             potatofile.write(potatofile_string)
             potatofile.close()
 
-            return [True, self.potato, KEYS]
+            return potato_returns().htato().HtatoPlantReturn(complete=True, path=self.potato, keys=KEYS)
 
         @CheckPotatoNonExistant
         def STAIN(self) -> dict:
@@ -127,6 +166,9 @@ class POTATO():
             potatofile.write(potatofile_string)
             potatofile.close()
             return [True, DATA, stained]
-        
 
-potato = POTATO()
+POTATO = POTATO()
+
+print(POTATO.STATO('.\\test.stato').STAIN(starch='config'))
+# returned {'version_history': True, 'encryption': False}
+# yipee
