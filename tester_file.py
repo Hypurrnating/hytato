bbebb6b478575a080ee68d5a80d3f4660d387136
Hypurrnating <imadyounis2004@gmail.com>
2023-07-20 15:30:40 +0300

    tester

--- tester_file.py
+++ tester_file.py
@@ -8,5 +8,5 @@ print(STATO.INJECT(data={'dsadsad': 43}))
 print(STATO.STAIN())
 
 print(HTATO.PLANT(['wee', 'weee', 'weeee']))
-print(HTATO.INJECT(data={'dsadsad': 43}))
+print(HTATO.INJECT(data={'wee': 43}))
 print(HTATO.STAIN())
