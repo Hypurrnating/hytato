dd18a46e4dded264b74cfed0d96b30009fc2df24
Hypurrnating <imadyounis2004@gmail.com>
2023-07-22 16:39:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 import setuptools; import re
 
+with open('README.md', 'r') as file:
+    readme = file.read()
+    file.close()
+
 setuptools.setup(
     name='hytato',
     description='Module for storing python dictionaries in a different format.',
-    version='0.2',
-    long_description='https://github.com/Hypurrnating/potato#readme',
+    long_description=readme,
     author='Hypurrnating',
     url='https://github.com/Hypurrnating/potato',
     license="MIT",
     packages=setuptools.find_packages(),
     classifiers=[   "Programming Language :: Python :: 3",
                     "License :: OSI Approved :: MIT License",
-                    "Operating System :: OS Independent"],
+                    "Operating System :: Microsoft :: Windows",
+                    "Operating System :: Unix"],
     python_requires=">=3.2",
     install_requires=[],
     include_package_data=True,
